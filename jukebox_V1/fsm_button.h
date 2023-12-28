9db3f94a69f28087ce5a1814a7ca8a39d75f7227
josueportiz <josueportiz@gmail.com>
2023-12-28 13:00:47 +0100

    File squeletons for version V1

---
+++ jukebox_V1/fsm_button.h
@@ -0,0 +1,22 @@
+/**
+ * @file fsm_button.h
+ * @brief Header for fsm_button.c file.
+ * @author alumno1
+ * @author alumno2
+ * @date fecha
+ */
+
+#ifndef FSM_BUTTON_H_
+#define FSM_BUTTON_H_
+
+/* Includes ------------------------------------------------------------------*/
+/* Standard C includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Other includes */
+#include "fsm.h"
+
+/* Function prototypes and explanation -------------------------------------------------*/
+
+#endif
---
+++ jukebox_V1/port_button.h
@@ -0,0 +1,27 @@
+/**
+ * @file port_button.h
+ * @brief Header for port_button.c file.
+ * @author alumno1
+ * @author alumno2
+ * @date fecha
+ */
+
+#ifndef PORT_BUTTON_H_
+#define PORT_BUTTON_H_
+
+/* Includes ------------------------------------------------------------------*/
+/* Standard C includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+/* HW dependent includes */
+
+
+/* Defines and enums ----------------------------------------------------------*/
+/* Defines */
+
+
+/* Function prototypes and explanation -------------------------------------------------*/
+
+
+#endif
