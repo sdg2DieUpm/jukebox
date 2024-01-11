cef5e9150e818a9837a4af3809e26691d3791a48
josueportiz <josueportiz@gmail.com>
2024-01-11 18:03:27 +0100

    Base files for system integration of Version 2.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jukebox Version 2 support files (system integration)
+
+This folder contains the support files for the system integration of the Jukebox Version 2 project.
+
+**Attention**
+
+The files are not organized in directories. The student must place them correctly in the `common` folder. Header files `.h` must be placed in `include` and source files `.c` must be placed in `src`.
---
+++ fsm_jukebox.h
@@ -0,0 +1,32 @@
+/**
+ * @file fsm_jukebox.h
+ * @brief Header for fsm_jukebox.c file.
+ * @author alumno1
+ * @author alumno2
+ * @date fecha
+ */
+#ifndef FSM_JUKEBOX_H_
+#define FSM_JUKEBOX_H_
+
+/* Includes ------------------------------------------------------------------*/
+/* Standard C includes */
+
+
+/* Other includes */
+
+
+/* Defines and enums ----------------------------------------------------------*/
+/* Defines */
+
+
+/* Enums */
+
+
+/* Typedefs ------------------------------------------------------------------*/
+
+
+/* Function prototypes and explanation ---------------------------------------*/
+
+
+
+#endif /* FSM_JUKEBOX_H_ */
